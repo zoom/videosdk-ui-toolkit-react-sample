da50b9bc407c243c5b062c42cf1ed9e6bfe0cece
Enda Zhang <enda.zhang@zoom.us>
2025-03-13 15:47:17 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,55 @@
-import uitoolkit from "@zoom/videosdk-ui-toolkit";
+import uitoolkit, { CustomizationOptions } from "@zoom/videosdk-ui-toolkit";
 import "@zoom/videosdk-ui-toolkit/dist/videosdk-ui-toolkit.css";
 import "./App.css";
 
 function App() {
   let sessionContainer: HTMLDivElement | null = null;
-  // set your auth endpoint here 
+  // set your auth endpoint here
   // a sample is available here: https://github.com/zoom/videosdk-auth-endpoint-sample
   const authEndpoint = ""; // http://localhost:4000
-  const config = {
+  const config: CustomizationOptions = {
     videoSDKJWT: "",
     sessionName: "test",
     userName: "React",
     sessionPasscode: "123",
-    features: ["video", "audio", "settings", "users", "chat", "share"],
-    options: { init: {}, audio: {}, video: {}, share: {} },
-    virtualBackground: {
-      allowVirtualBackground: true,
-      allowVirtualBackgroundUpload: true,
-      virtualBackgrounds: ['https://images.unsplash.com/photo-1715490187538-30a365fa05bd?q=80&w=1945&auto=format&fit=crop']
-    }
+    featuresOptions: {
+      virtualBackground: {
+        enable: true,
+        virtualBackgrounds: [
+          {
+            url: "https://images.unsplash.com/photo-1715490187538-30a365fa05bd?q=80&w=1945&auto=format&fit=crop",
+          },
+        ],
+      },
+    },
   };
   const role = 1;
 
   function getVideoSDKJWT() {
-    sessionContainer = document.getElementById("sessionContainer") as HTMLDivElement;
+    sessionContainer = document.getElementById(
+      "sessionContainer"
+    ) as HTMLDivElement;
     document.getElementById("join-flow")!.style.display = "none";
     fetch(authEndpoint, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ sessionName: config.sessionName, role: role, }),
-    }).then((response) => {
-      return response.json();
-    }).then((data) => {
-      if (data.signature) {
-        console.log(data.signature);
-        config.videoSDKJWT = data.signature;
-        joinSession();
-      } else {
-        console.log(data);
-      }
-    }).catch((error) => {
-      console.log(error);
-    });
+      body: JSON.stringify({ sessionName: config.sessionName, role: role }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        if (data.signature) {
+          console.log(data.signature);
+          config.videoSDKJWT = data.signature;
+          joinSession();
+        } else {
+          console.log(data);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   function joinSession() {
