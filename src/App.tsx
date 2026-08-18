7977e3ae90eed71addb2747e3179bb377275104f
Joyce Ma <joyce.ma@zoom.us>
2026-08-18 14:21:57 -0700

    Remove authEndpoint

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   let sessionContainer: HTMLDivElement | null = null;
   // set your auth endpoint here
   // a sample is available here: https://github.com/zoom/videosdk-auth-endpoint-sample
-  const authEndpoint = 'http://localhost:4000'; // http://localhost:4000
+  const authEndpoint = ''; // http://localhost:4000
   const config: CustomizationOptions = {
     videoSDKJWT: '',
     sessionName: 'test',
