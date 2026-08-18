d3c5941f945ef1edd2269733a9e7bdf961632f2f
Joyce Ma <43423318+PikaJoyce@users.noreply.github.com>
2026-08-18 14:23:50 -0700

    2.5.0 1 (#26)
    * 2.5.0-1
    
    * Remove authEndpoint
    
    ---------
    
    Co-authored-by: Joyce Ma <joyce.ma@zoom.us>

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import uitoolkit, { CustomizationOptions } from "@zoom/videosdk-ui-toolkit";
-import "@zoom/videosdk-ui-toolkit/dist/videosdk-ui-toolkit.css";
-import "./App.css";
+import uitoolkit, { CustomizationOptions } from '@zoom/videosdk-ui-toolkit';
+import '@zoom/videosdk-ui-toolkit/dist/videosdk-ui-toolkit.css';
+import './App.css';
 
 function App() {
   let sessionContainer: HTMLDivElement | null = null;
   // set your auth endpoint here
   // a sample is available here: https://github.com/zoom/videosdk-auth-endpoint-sample
-  const authEndpoint = ""; // http://localhost:4000
+  const authEndpoint = ''; // http://localhost:4000
   const config: CustomizationOptions = {
-    videoSDKJWT: "",
-    sessionName: "test",
-    userName: "React",
-    sessionPasscode: "123",
+    videoSDKJWT: '',
+    sessionName: 'test',
+    userName: 'React',
+    sessionPasscode: '123',
     featuresOptions: {
       preview: {
         enable: true,
@@ -20,7 +20,7 @@ function App() {
         enable: true,
         virtualBackgrounds: [
           {
-            url: "https://images.unsplash.com/photo-1715490187538-30a365fa05bd?q=80&w=1945&auto=format&fit=crop",
+            url: 'https://images.unsplash.com/photo-1715490187538-30a365fa05bd?q=80&w=1945&auto=format&fit=crop',
           },
         ],
       },
@@ -30,12 +30,12 @@ function App() {
 
   function getVideoSDKJWT() {
     sessionContainer = document.getElementById(
-      "sessionContainer"
+      'sessionContainer',
     ) as HTMLDivElement;
-    document.getElementById("join-flow")!.style.display = "none";
+    document.getElementById('join-flow')!.style.display = 'none';
     fetch(authEndpoint, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         sessionName: config.sessionName,
         role: role,
@@ -69,24 +69,24 @@ function App() {
   }
 
   const sessionClosed = () => {
-    console.log("session closed");
-    document.getElementById("join-flow")!.style.display = "block";
+    console.log('session closed');
+    document.getElementById('join-flow')!.style.display = 'block';
   };
 
   const sessionDestroyed = () => {
-    console.log("session destroyed");
+    console.log('session destroyed');
     uitoolkit.destroy();
   };
 
   return (
-    <div className="App">
+    <div className='App'>
       <main>
-        <div id="join-flow">
+        <div id='join-flow'>
           <h1>Zoom Video SDK Sample React</h1>
           <p>User interface offered by the Video SDK UI Toolkit</p>
           <button onClick={getVideoSDKJWT}>Join Session</button>
         </div>
-        <div id="sessionContainer"></div>
+        <div id='sessionContainer'></div>
       </main>
     </div>
   );
